9c19104868a2475a8e1785b30ef74157d1cdef15
Jonathan Beard <jonathan.c.beard@gmail.com>
2015-03-17 13:11:39 -0500

    updates

--- TestCases/portTypeException.cpp
+++ TestCases/portTypeException.cpp
@@ -3,9 +3,41 @@
 #include <cstdint>
 #include <cstdlib>
 #include <raft>
+#include <raftio>
 
 
-template< typename A, typename B, typename C > class sum : public raft::kernel
+template < typename T > class Generate : public raft::kernel
+{
+public:
+   Generate( std::int64_t count = 1000 ) : raft::kernel(),
+                                         count( count )
+   {
+      output.addPort< T >( "number_stream" );
+   }
+
+   virtual raft::kstatus run()
+   {
+      if( count-- > 1 )
+      {
+         
+         auto &ref( output[ "number_stream" ].allocate< T >() );
+         ref = count;
+         output[ "number_stream"].send();
+         
+         return( raft::proceed );
+      }
+      /** else **/
+      auto &ref( output[ "number_stream" ].allocate< T >() );
+      ref = count;
+      output[ "number_stream" ].send( raft::eof );
+      return( raft::stop );
+   }
+
+private:
+   std::int64_t count;
+};
+
+template< typename A, typename B, typename C > class Sum : public raft::kernel
 {
 public:
    Sum() : raft::kernel()
@@ -38,13 +70,24 @@ public:
 int
 main( int argc, char **argv )
 {
-   using namespace raft;
-   Map map;
-   auto linked_kernels( map.link( new Generate< float >(),
-                                  new Sum< std::int64_t,std::int64_t, std::int64_t >(),
-                                  "input_a" ) );
-   map.link( new Generate< std::int64_t >(), &( linked_kernels.dst ), "input_b" );
-   map.link( &( linked_kernels.dst ), new Print< std::int64_t >() );
-   map.exe();
+   int count( 1000 );
+   if( argc == 2 )
+   {
+      count = atoi( argv[ 1 ] );
+   }
+   using gen = Generate< std::int64_t >;
+   using sum = Sum< std::int64_t, float, std::int64_t >;
+   using p_out = raft::print< std::int64_t, '\n' >;
+
+   auto linked_kernels( 
+      raft::map.link( raft::kernel::make< gen >( count ),
+                      raft::kernel::make< sum >(), "input_a" ) );
+   raft::map.link( 
+      raft::kernel::make< gen >( count ),
+      &linked_kernels.getDst(), "input_b"  );
+   raft::map.link( 
+      &linked_kernels.getDst(), 
+      raft::kernel::make< p_out >() );
+   raft::map.exe();
    return( EXIT_SUCCESS );
 }
