f66045c151bd82184b37771c4244b0462c140a24
Jonathan Beard <jonathan.c.beard@gmail.com>
2015-12-02 07:48:34 -0600

    updates, should have fixed previous broken commit

--- raft_dir/ringbuffertypes.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __RINGBUFFERTYPES__ 
-#define __RINGBUFFERTYPES__ 1
-namespace Type{
-   enum RingBufferType { Heap, SharedMemory, TCP, Infinite, N};
-}
-   
-   enum Direction { Producer, Consumer };
-#endif
