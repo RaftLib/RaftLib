aa31524cdd0b4d2b80b95114f715949b3703efff
Jonathan Beard <jonathan.c.beard@gmail.com>
2016-07-24 20:08:40 -0500

    updates, initial commit to qthreads

--- cmake/CheckGitDep.cmake
+++ cmake/CheckGitDep.cmake
@@ -5,10 +5,12 @@ set( DEPDIR ${CMAKE_SOURCE_DIR}/git-dep )
 ##
 # LIST MODULES HERE
 ##
-set( GIT_MODULES cmdargs shm )
+set( GIT_MODULES cmdargs shm qthreads )
 ##
 # NOW CHECK THEM OUT 
 ##
+include(ExternalProject)
+
 foreach( GMOD ${GIT_MODULES} )
  message( INFO " Initializing sub-module ${DEPDIR}/${GMOD} from git repo!" )
  execute_process( COMMAND git submodule init ${DEPDIR}/${GMOD}
@@ -20,6 +22,16 @@ foreach( GMOD ${GIT_MODULES} )
  ##
  if( EXISTS ${DEPDIR}/${GMOD}/CMakeLists.txt )
     add_subdirectory( ${DEPDIR}/${GMOD} )
+ elseif( EXISTS ${DEPDIR}/${GMOD}/autogen.sh )
+    message( INFO " Found automake dir in git, attempting to incorporate..." )
+    ExternalProject_Add( ${GMOD}
+        SOURCE_DIR ${DEPDIR}/${GMOD}
+        CONFIGURE_COMMAND ${DEPDIR}/${GMOD}/autogen.sh && ${DEPDIR}/${GMOD}/configure --prefix=${DEPDIR}/${GMOD}
+#        INSTALL_DIR ${DEPDIR}/${GMOD}/build
+        TEST_COMMAND make test
+        BUILD_COMMAND make
+        TEST_BEFORE_INSTALL 1
+        BUILD_IN_SOURCE 1 )
  endif( EXISTS ${DEPDIR}/${GMOD}/CMakeLists.txt )
  ##
  # assume we have an include dir in the sub-module
