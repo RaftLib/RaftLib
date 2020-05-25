7034e93a2ab8f42a042319543d48d390f7031101
Jonathan Beard <jonathan.c.beard@gmail.com>
2020-05-24 21:12:37 -0500

    moved aligncode to its own file

--- raftinc/blocked.hpp
+++ raftinc/blocked.hpp
@@ -21,59 +21,7 @@
 #define BLOCKED_HPP  1
 #include <cstdint>
 #include <cassert>
-
-
-/** 
- * FIXME move thos code to the defs.hpp file
- * useful for other things, no point in duplicating
- * elsewhere 
- */
-#ifdef _MSC_VER
-#    if (_MSC_VER >= 1800)
-#        define __alignas_is_defined 1
-#    endif
-#    if (_MSC_VER >= 1900)
-#        define __alignof_is_defined 1
-#    endif
-#else 
-#    ifndef __APPLE__
-#    include <cstdalign>   // __alignas/of_is_defined directly from the implementation
-#    endif
-#endif
-
-/**
- * should be included, but just in case there are some 
- * compilers with only experimental C++11 support still
- * running around, check macro..turns out it's #ifdef out 
- * on GNU G++ so checking __cplusplus flag as indicated
- * by https://goo.gl/JD4Gng
- */
-#if ( __alignas_is_defined == 1 ) || ( __cplusplus >= 201103L )
-#    define ALIGN(X) alignas(X)
-#else
-#    pragma message("C++11 alignas unsupported :( Falling back to compiler attributes")
-#    ifdef __GNUG__
-#        define ALIGN(X) __attribute__ ((aligned(X)))
-#    elif defined(_MSC_VER)
-#        define ALIGN(X) __declspec(align(X))
-#    else
-#        error Unknown compiler, unknown alignment attribute!
-#    endif
-#endif
-
-#if ( __alignas_is_defined == 1 ) || ( __cplusplus >= 201103L )
-#    define ALIGNOF(X) alignof(x)
-#else
-#    pragma message("C++11 alignof unsupported :( Falling back to compiler attributes")
-#    ifdef __GNUG__
-#        define ALIGNOF(X) __alignof__ (X)
-#    elif defined(_MSC_VER)
-#        define ALIGNOF(X) __alignof(X)
-#    else
-#        error Unknown compiler, unknown alignment attribute!
-#    endif
-#endif
-
+#include "internaldefs.hpp"
 
 /**
  * FIXME...should probably align these to cache line then 
